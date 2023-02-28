101f6291d26e460769df5ecc8fd8d3ce99dafba4
David Llobell <dllobellmoya@gmail.com>
2023-02-28 20:09:42 +0100

    Update config file

--- config/hubspot.php
+++ config/hubspot.php
@@ -7,21 +7,11 @@ return [
         'main' => [
             'config' => [
                 'key' => '',
+                'oauth' => true,
             ],
             'retry_middleware' => [
                 'rate_limit' => 'constant:5',
                 'internal_errors' => 'exponential:2'
-            ],
-            'client_options' => [
-                'http_errors' => true,
-            ],
-            'wrap_response' => true,
-        ],
-
-        'alternative' => [
-            'config' => [
-                'key' => '',
-                'oauth' => true,
             ],
             'client_options' => [
                 'http_errors' => true,
